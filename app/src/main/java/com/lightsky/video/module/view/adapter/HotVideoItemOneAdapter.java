0124b4f73be997960d1165b5b18d3632166fb51f
lanhuawei <974801031@qq.com>
2018-07-30 17:52:13 +0800

    更新

--- app/src/main/java/com/lightsky/video/module/view/adapter/HotVideoItemOneAdapter.java
+++ app/src/main/java/com/lightsky/video/module/view/adapter/HotVideoItemOneAdapter.java
@@ -6,14 +6,18 @@ import android.view.ViewGroup;
 import com.lightsky.video.R;
 import com.lightsky.video.common.mywidget.recyclerview.adapter.BaseRecyclerAdapter;
 import com.lightsky.video.common.mywidget.util.ViewInflate;
+import com.lightsky.video.common.videoplayer.player.IjkVideoView;
 import com.lightsky.video.module.entity.databean.MainVideoDataBean;
 import com.lightsky.video.module.view.holder.HotVideoItemOneHolder;
+
+import butterknife.BindView;
 
 /**
  * Created by Ivan.L on 2018/7/27.
  * 热门tab one精选 Adapter
  */
 public class HotVideoItemOneAdapter extends BaseRecyclerAdapter<MainVideoDataBean, HotVideoItemOneHolder> {
+
 
     public HotVideoItemOneAdapter(Context context, OnItemClickListener<HotVideoItemOneHolder> listener) {
         super(context, listener);
--- app/src/main/java/com/lightsky/video/module/view/holder/HotVideoItemOneHolder.java
+++ app/src/main/java/com/lightsky/video/module/view/holder/HotVideoItemOneHolder.java
@@ -1,23 +1,51 @@
 package com.lightsky.video.module.view.holder;
 
+import android.content.Context;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.lightsky.video.R;
+import com.lightsky.video.common.customview.CircleImageView;
 import com.lightsky.video.common.mywidget.recyclerview.adapter.BaseRecyclerViewHolder;
+import com.lightsky.video.common.videoplayer.controller.StandardVideoController;
+import com.lightsky.video.common.videoplayer.player.IjkVideoView;
+import com.lightsky.video.common.videoplayer.player.PlayerConfig;
 import com.lightsky.video.module.entity.databean.MainVideoDataBean;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Ivan.L on 2018/7/27.
  * 热门tab one精选 Holder
  */
 public class HotVideoItemOneHolder extends BaseRecyclerViewHolder<MainVideoDataBean> {
+    @BindView(R.id.ijk_videoview) IjkVideoView ijkVideoview;
+    @BindView(R.id.iv_user_avatar) CircleImageView ivUserAvatar;
+    @BindView(R.id.tv_username) TextView tvUsername;
+    @BindView(R.id.tv_play_count) TextView tvPlayCount;
+    @BindView(R.id.rl_bottom) RelativeLayout rlBottom;
+    private StandardVideoController controller;
+    private PlayerConfig mPlayerConfig;
+    Context mContext;
 
     public HotVideoItemOneHolder(View view) {
         super(view);
+        ButterKnife.bind(this, view);
+        mContext = view.getContext();
+        controller = new StandardVideoController(view.getContext());
+        ijkVideoview.setVideoController(controller);
+        mPlayerConfig = new PlayerConfig.Builder()
+                .addToPlayerManager()
+                .build();
     }
 
     @Override
     protected void onItemDataUpdated(@Nullable MainVideoDataBean mainVideoDataBean) {
+        if (mainVideoDataBean != null) {
 
+        }
     }
 }
